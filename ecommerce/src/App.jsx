74d0dd51530dfe6cb02af2e72b9b5e0268d5b1ec
Loana Parra <loa.parra01@gmail.com>
2024-10-23 19:55:08 -0300

    adelanto de proyecto

---
+++ ecommerce/src/App.jsx
@@ -0,0 +1,15 @@
+import ItemListContainer from "./components/itemListContainer/itemListContainer"
+import NavBar from "./components/NavBar/NavBar"
+
+
+function App() {
+
+  return (
+   <>
+   <NavBar/>
+   <ItemListContainer/>
+   </>
+  )
+}
+
+export default App
